5b312cbdf769afd8218195a600a713d32ab53395
tanushreemarwah <tanushreemarwah1@gmail.com>
2020-07-25 23:07:51 +0530

    Create form to add two numbers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { render } from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class AddTwoNumbers extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = (event) => {
+    let input = event.target.name;
+    let value = event.target.value;
+    if (!Number(value)){
+      alert("You must input a number");
+    }
+    this.setState({[input]: value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let input1 = parseInt(this.refs.input1.value);
+    let input2 = parseInt(this.refs.input2.value);
+    if ( !Number(input1) || !Number(input2)){
+      alert("You must enter two numbers!");
+    } else {
+      this.setState({value: (input1 + input2)});
+    }
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="number">
+          <label>
+            Enter First Number: 
+            <input id="input1" ref="input1" name="input1" type="text" pattern="[0-9]*" value={this.state.input1} onChange={this.handleChange} />
+          </label>
+          <label>
+            Enter Second Number: 
+            <input id="input2" ref="input2" name="input2" type="text"  pattern="[0-9]*" value={this.state.input2} onChange={this.handleChange} />
+          </label>
+          <label>
+            Result: 
+            <input id="result" type="text" name="result" ref="result" value={this.state.value} readOnly={true}/>
+          </label>
+          <br/>
+          <input id="submit-button" name="click-button" type="submit" disabled={!this.state.input1 || !this.state.input2} value="Display Result" />
+        </div>
+      </form>
+    );
+  }
 }
 
-export default App;
+export default AddTwoNumbers;
